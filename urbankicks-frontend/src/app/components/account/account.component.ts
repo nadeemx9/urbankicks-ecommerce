1349c86df249c31f836634ca593a76d70379a2c5
nadeem_palkhiwala <nadeem.palkhiwala@xomicmail.com>
2024-09-21 17:47:39 +0530

    UI changes

--- urbankicks-frontend/src/app/components/account/account.component.ts
+++ urbankicks-frontend/src/app/components/account/account.component.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-account',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterModule
+  ],
   templateUrl: './account.component.html',
   styleUrl: './account.component.scss'
 })
 export class AccountComponent {
 
   constructor(
-    titleService : Title
-  ){
+    titleService: Title
+  ) {
     titleService.setTitle('UrbanKicks - Account')
   }
 
